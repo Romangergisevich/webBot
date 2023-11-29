8af444301408c119c2994aaa408be28389b62741
unknown <Romangergisevich@gmail.com>
2023-11-29 05:44:48 +0300

    predeploy config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: `/simpleBotHelper/`,
+  publicPath: `/webBot/`,
   transpileDependencies: true,
 });
